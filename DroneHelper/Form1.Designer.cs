2fc09ad1569cb977b0a2314f94d4fde5cc1e6435
Kuvaev-dev <likeaboss13371@gmail.com>
2024-10-13 21:49:14 +0300

    Добавьте файлы проекта.

---
+++ DroneHelper/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace DroneHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSaveMap = new System.Windows.Forms.Button();
+            this.btnGenerateEnemies = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxBefore = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxAfter = new System.Windows.Forms.PictureBox();
+            this.lstDroneCoordinates = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(96, 21);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(128, 20);
+            this.txtRadius.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть радіус:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(230, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(135, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSaveMap);
+            this.groupBox1.Controls.Add(this.btnGenerateEnemies);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtRadius);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(762, 56);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Дані користувача";
+            // 
+            // btnSaveMap
+            // 
+            this.btnSaveMap.Location = new System.Drawing.Point(577, 19);
+            this.btnSaveMap.Name = "btnSaveMap";
+            this.btnSaveMap.Size = new System.Drawing.Size(179, 23);
+            this.btnSaveMap.TabIndex = 4;
+            this.btnSaveMap.Text = "Зберегти мапу та координати";
+            this.btnSaveMap.UseVisualStyleBackColor = true;
+            this.btnSaveMap.Click += new System.EventHandler(this.btnSaveMap_Click);
+            // 
+            // btnGenerateEnemies
+            // 
+            this.btnGenerateEnemies.Location = new System.Drawing.Point(371, 19);
+            this.btnGenerateEnemies.Name = "btnGenerateEnemies";
+            this.btnGenerateEnemies.Size = new System.Drawing.Size(135, 23);
+            this.btnGenerateEnemies.TabIndex = 3;
+            this.btnGenerateEnemies.Text = "Згенерувати ворогів";
+            this.btnGenerateEnemies.UseVisualStyleBackColor = true;
+            this.btnGenerateEnemies.Click += new System.EventHandler(this.btnGenerateEnemies_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBoxBefore);
+            this.groupBox2.Location = new System.Drawing.Point(12, 74);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(378, 364);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Мапа розміщення ворогів";
+            // 
+            // pictureBoxBefore
+            // 
+            this.pictureBoxBefore.Location = new System.Drawing.Point(6, 19);
+            this.pictureBoxBefore.Name = "pictureBoxBefore";
+            this.pictureBoxBefore.Size = new System.Drawing.Size(366, 339);
+            this.pictureBoxBefore.TabIndex = 0;
+            this.pictureBoxBefore.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBoxAfter);
+            this.groupBox3.Location = new System.Drawing.Point(396, 74);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(378, 364);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Мапа потенційного розміщення дронів";
+            // 
+            // pictureBoxAfter
+            // 
+            this.pictureBoxAfter.Location = new System.Drawing.Point(6, 19);
+            this.pictureBoxAfter.Name = "pictureBoxAfter";
+            this.pictureBoxAfter.Size = new System.Drawing.Size(366, 339);
+            this.pictureBoxAfter.TabIndex = 1;
+            this.pictureBoxAfter.TabStop = false;
+            // 
+            // lstDroneCoordinates
+            // 
+            this.lstDroneCoordinates.FormattingEnabled = true;
+            this.lstDroneCoordinates.Location = new System.Drawing.Point(9, 19);
+            this.lstDroneCoordinates.Name = "lstDroneCoordinates";
+            this.lstDroneCoordinates.Size = new System.Drawing.Size(745, 82);
+            this.lstDroneCoordinates.TabIndex = 2;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lstDroneCoordinates);
+            this.groupBox4.Location = new System.Drawing.Point(14, 453);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(762, 110);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Координати дронів";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(786, 576);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Помічник пілота дронами";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBoxBefore;
+        private System.Windows.Forms.PictureBox pictureBoxAfter;
+        private System.Windows.Forms.ListBox lstDroneCoordinates;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnSaveMap;
+        private System.Windows.Forms.Button btnGenerateEnemies;
+    }
+}
+
---
+++ DroneHelper/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DroneHelper
+{
+    public partial class Form1 : Form
+    {
+        // Генератор випадкових чисел
+        private static readonly Random _rnd = new Random();
+
+        // Константи для налаштування алгоритму
+        const int GenerationSize = 100; // Розмір покоління
+        const int GenerationNumbers = 200; // Кількість поколінь
+        const double MutationProbability = 0.2; // Імовірність мутації
+        const int NumberOfDrones = 10; // Кількість дронів
+        static double Radius; // Радіус дії дронів
+
+        // Список ворогів, де кожен ворог представлений координатами (x, y)
+        static readonly List<(double, double)> enemies = new List<(double, double)>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Генерація випадкової кількості ворогів при запуску форми
+            GenerateEnemies(_rnd.Next(30, 100));
+
+            // Встановлення фіксованого розміру форми
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        // Обробник події для кнопки "Почати"
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput()) // Перевірка введених даних
+                return;
+
+            Radius = Convert.ToDouble(txtRadius.Text); // Зчитування радіуса з текстового поля
+            List<KeyValuePair<int[], double>> generation = GenerateRandom(); // Генерація початкового покоління
+            SortGeneration(generation); // Сортування покоління за пристосованістю
+
+            // Генерація нових поколінь
+            for (int getNum = 0; getNum < GenerationNumbers; getNum++)
+            {
+                generation = GenerateNewGeneration(generation, true);
+                SortGeneration(generation);
+            }
+
+            int[] bestGenome = generation[0].Key; // Отримання найкращого геному
+            DrawDrones(bestGenome, pictureBoxAfter); // Відображення дронів на карті
+        }
+
+        // Обробник події для кнопки "Згенерувати ворогів"
+        private void btnGenerateEnemies_Click(object sender, EventArgs e)
+        {
+            GenerateEnemies(_rnd.Next(30, 100)); // Генерація нових ворогів
+        }
+
+        // Обробник події для кнопки "Зберегти мапу"
+        private void btnSaveMap_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "PNG Image|*.png";
+                sfd.Title = "Зберегти мапу";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    // Збереження зображення карти у файл
+                    Bitmap bmp = new Bitmap(pictureBoxAfter.Width, pictureBoxAfter.Height);
+                    pictureBoxAfter.DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                    bmp.Save(sfd.FileName);
+
+                    // Збереження координат дронів у текстовий файл
+                    string textFileName = System.IO.Path.ChangeExtension(sfd.FileName, ".txt");
+                    using (StreamWriter sw = new StreamWriter(textFileName))
+                    {
+                        foreach (var item in lstDroneCoordinates.Items)
+                        {
+                            sw.WriteLine(item.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        // Перевірка введених даних
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtRadius.Text))
+            {
+                MessageBox.Show("Будь ласка, введіть радіус.", "Помилка вводу", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!double.TryParse(txtRadius.Text, out double radius) || radius <= 0)
+            {
+                MessageBox.Show("Будь ласка, введіть дійсне додатне число для радіуса.", "Помилка вводу", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        // Генерація випадкових ворогів
+        // Параметр 100.0 можна змінити для більшої розсіяності ворогів
+        private void GenerateEnemies(int count)
+        {
+            enemies.Clear(); // Очищення списку ворогів
+            for (int i = 0; i < count; i++)
+            {
+                double x = _rnd.NextDouble() * 100.0; // Випадкова координата X
+                double y = _rnd.NextDouble() * 100.0; // Випадкова координата Y
+                enemies.Add((x, y)); // Додавання ворога до списку
+            }
+            DrawEnemies(pictureBoxBefore); // Відображення ворогів на карті
+        }
+
+        // Відображення ворогів на карті
+        private void DrawEnemies(PictureBox pictureBox)
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White); // Очищення зображення
+                foreach (var enemy in enemies)
+                {
+                    g.FillEllipse(Brushes.Red, (float)enemy.Item1, (float)enemy.Item2, 5, 5); // Малювання ворога
+                }
+            }
+            pictureBox.Image = bmp; // Встановлення зображення на PictureBox
+        }
+
+        // Відображення дронів на карті
+        private void DrawDrones(int[] bestGenome, PictureBox pictureBox)
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White); // Очищення зображення
+                foreach (var enemy in enemies)
+                {
+                    g.FillEllipse(Brushes.Red, (float)enemy.Item1, (float)enemy.Item2, 5, 5); // Малювання ворогів }
+
+                    lstDroneCoordinates.Items.Clear(); // Очищення списку координат дронів
+                    for (int i = 0; i < NumberOfDrones; i++)
+                    {
+                        double x = GetX(bestGenome[i]); // Отримання координати X дрона
+                        double y = GetY(bestGenome[i]); // Отримання координати Y дрона
+                        g.FillEllipse(Brushes.Blue, (float)x, (float)y, 5, 5); // Малювання дрона
+                        g.DrawEllipse(Pens.Blue, (float)(x - Radius), (float)(y - Radius), (float)(Radius * 2), (float)(Radius * 2)); // Малювання радіуса дії дрона
+
+                        lstDroneCoordinates.Items.Add($"Дрон {i + 1}: X = {x}, Y = {y}"); // Додавання координат дрона до списку
+                    }
+                }
+                pictureBox.Image = bmp; // Встановлення зображення на PictureBox
+            }
+        }
+
+        // Генерація випадкових геномів для початкового покоління
+        private List<KeyValuePair<int[], double>> GenerateRandom()
+        {
+            List<KeyValuePair<int[], double>> result = new List<KeyValuePair<int[], double>>();
+            Parallel.For(0, GenerationSize, i =>
+            {
+                int[] genome = new int[NumberOfDrones];
+                for (int j = 0; j < NumberOfDrones; j++)
+                {
+                    genome[j] = _rnd.Next(); // Генерація випадкового геному
+                }
+                double fitness = Fitness(genome);
+                lock (result) // Забезпечення потокобезпечності при додаванні до списку {
+                    result.Add(new KeyValuePair<int[], double>(genome, fitness));
+            });
+            return result;
+        }
+
+        // Сортування покоління за пристосованістю
+        private void SortGeneration(List<KeyValuePair<int[], double>> generation)
+        {
+            generation.Sort((x, y) => y.Value.CompareTo(x.Value)); // Сортування за спаданням пристосованості
+            if (generation.Count > GenerationSize)
+                generation.RemoveRange(GenerationSize, generation.Count - GenerationSize); // Видалення зайвих елементів
+        }
+
+        // Обчислення координати Y з геному
+        // Параметр 100 можна змінити для більшої розсіяності ворогів
+        private double GetY(int genome)
+        {
+            int y = genome & 0xffff;
+            return y * 100.0 / 0x10000;
+        }
+
+        // Обчислення координати X з геному
+        // Параметр 100.0 можна змінити для більшої розсіяності ворогів
+        private double GetX(int genome)
+        {
+            int x = (genome >> 16) & 0xffff;
+            return x * 100.0 / 0x10000;
+        }
+
+        // Обчислення пристосованості геному
+        private double Fitness(int[] genome)
+        {
+            HashSet<int> destroyedEnemies = new HashSet<int>();
+
+            for (int i = 0; i < NumberOfDrones; i++)
+            {
+                double droneX = GetX(genome[i]);
+                double droneY = GetY(genome[i]);
+
+                for (int j = 0; j < enemies.Count; j++)
+                {
+                    double enemyX = enemies[j].Item1;
+                    double enemyY = enemies[j].Item2;
+
+                    if (Math.Sqrt(Math.Pow(droneX - enemyX, 2) + Math.Pow(droneY - enemyY, 2)) <= Radius)
+                    {
+                        destroyedEnemies.Add(j); // Додавання індексу знищеного ворога
+                    }
+                }
+            }
+
+            return destroyedEnemies.Count; // Кількість знищених ворогів як значення пристосованості
+        }
+
+        // Генерація нового покоління з батьків
+        private List<KeyValuePair<int[], double>> GenerateNewGeneration(List<KeyValuePair<int[], double>> parents, bool useElitism)
+        {
+            List<KeyValuePair<int[], double>> result = new List<KeyValuePair<int[], double>>();
+
+            if (useElitism)
+                result.Add(parents[0]); // Додавання найкращого геному з попереднього покоління
+
+            Parallel.For(result.Count, GenerationSize, _ =>
+            {
+                int parent1 = _rnd.Next(GenerationSize);
+                int parent2 = _rnd.Next(GenerationSize);
+
+                int[] child = new int[NumberOfDrones];
+                for (int i = 0; i < NumberOfDrones; i++)
+                {
+                    int mask = ~0 << _rnd.Next(32);
+                    child[i] = parents[parent1].Key[i] & mask | parents[parent2].Key[i] & ~mask; // Кросовер генів
+
+                    if (_rnd.NextDouble() < MutationProbability)
+                        child[i] ^= 1 << _rnd.Next(32); // Мутація гену
+                }
+
+                double fitness = Fitness(child);
+                lock (result) // Забезпечення потокобезпечності при додаванні до списку
+                {
+                    result.Add(new KeyValuePair<int[], double>(child, fitness));
+                }
+            });
+
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
